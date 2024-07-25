88e3c44b8941899c9717d785f3613c46a7d10aa3
ajajshwj <1475215798@qq.com>
2024-07-25 21:10:04 +0800

    第二次提交

---
+++ rootdir/bin/ondemand_gpu.sh
@@ -0,0 +1,15 @@
+#!/system/bin/sh
+
+freq_table=/data/gpu_freq_table.conf
+
+opp_list=$(tac /proc/gpufreqv2/stack_working_opp_table | awk '{print $3$5}' | sed "s/.$//" | sed "s/,/ /g")
+
+preset="# Freq Volt
+$opp_list
+"
+
+if [[ ! -f $freq_table ]]; then
+    echo "$preset" > $freq_table
+fi
+
+exec /vendor/bin/ondemand_mtk
